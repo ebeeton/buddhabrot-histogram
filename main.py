41db830539d410006f97480d1ac6e004419b80ab
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-01-27 18:00:48 -0500

    Use Pandas' series histogram.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
     data = []
     with open('counter.txt') as f:
         data = [int(line.strip()) for line in f]
 
-    n, bins, patches = plt.hist(x=data, bins='auto', color='#4287f5')
+    counts = pd.Series(data)
+    counts.plot.hist(grid=True, bins=50, rwidth=0.9, color='#4287f5')
     plt.savefig('histogram')
 
 if __name__ == '__main__':
